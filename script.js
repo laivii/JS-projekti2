bec62620458ed04ee6a1e1ca9b0578235d9ccc62
laivii <91199307+laivii@users.noreply.github.com>
2022-11-14 21:53:43 +0200

    testing things

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function loadDoc(name1, name2) {
         var responseObj = JSON.parse(this.responseText); //Parsing the json we get as a response, so we can use it
         console.log(responseObj); //Console log so we can look what we get
 
-        document.getElementById("person1").innerHTML = this.responseText; //This one is atm useless, but this one writes the thing into the site
+        document.getElementById("results").innerHTML = this.responseText; //This one is atm useless, but this one writes the thing into the site
     }
 
     //Here we are defining the names to be used in the calculation
-    sname = "Kalle"; //name1; //First name to be used in the calculatio
-    fname = "Pentti"; //name2; //Second name to be used in the calculation
+    sname = "V"; //name1; //First name to be used in the calculatio
+    fname = "V"; //name2; //Second name to be used in the calculation
 
     xhttp.open("GET", "https://love-calculator.p.rapidapi.com/getPercentage?sname="+sname+"&fname="+fname, true); //the site we are requesting
     xhttp.setRequestHeader("X-RapidAPI-Key", "5784b570e7msh46bcabeb3618464p179590jsn272f2a0f6e83"); //here we tell our API key
